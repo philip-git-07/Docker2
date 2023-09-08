e5219ebe7f238d258d49bbeedab210f19b79e280
pythonkid2 <mjcmathew@gmail.com>
2023-09-08 22:02:47 +0530

    war file added

---
+++ src/main/java/com/staragile/MySpringBootApplication/HomeController.java
@@ -0,0 +1,9 @@
+package com.staragile.MySpringBootApplication;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+    // Your controller code here
+}
